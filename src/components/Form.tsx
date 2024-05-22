01c7f975d3a152215554a3fc13a2e38583feecb8
Bereket Abera <Bereket.Abera@zemenbank.local>
2024-05-22 15:33:11 +0300

    Managing Validating Form Fields Using FormState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,24 +3,30 @@ import { FieldValues, useForm } from "react-hook-form"
 
 const Form = () => {
 
-  const {register, handleSubmit} = useForm();
 
-  const onSubmit = (val: FieldValues) => {
-    console.log(val)
-  };
+  interface Props{  
+    email: String,
+    password: String
+  }
+  const {register, handleSubmit, formState : {errors}} = useForm<Props>();
+  // formState : {errors} : Advanced Destructuring in JS
+
+  const onSubmit = (val: FieldValues) => console.log(val);
+
 
   return (
     <form onSubmit={handleSubmit( onSubmit )}>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
     <input 
-    {...register('Email')}
-    
+    {...register('email', { required: true, minLength : 3})}
     type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+   {errors.email?.type === 'required' && <p className="text-danger">The Email Field Is Required</p>}
+{   errors.email?.type === "minLength" && <p className="text-danger">The Email Field Must be atlease 3 Character</p>}
   </div>
   <div className="mb-3">
     <label  htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input  {...register('Password')} type="password" className="form-control" id="exampleInputPassword1" />
+    <input  {...register('password')} type="password" className="form-control" id="exampleInputPassword1" />
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
