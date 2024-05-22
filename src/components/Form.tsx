9762e7e144cf8648319adbbedcddeb70bfb733a2
Bereket Abera <Bereket.Abera@zemenbank.local>
2024-05-22 11:55:53 +0300

    Managing Forms using React Hook Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,26 @@
-import {FormEvent, useState } from "react"
+import { FieldValues, useForm } from "react-hook-form"
+
 
 const Form = () => {
 
-  const[ cridentials, setCridentials ]= useState(
-    {
-      username: '',
-      password: ''
-    }
-    
-  )
-  const handleSubmit = (e : FormEvent)=>{
-    e.preventDefault();
-    console.log(cridentials)
+  const {register, handleSubmit} = useForm();
 
-  } 
+  const onSubmit = (val: FieldValues) => {
+    console.log(val)
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit( onSubmit )}>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input value={cridentials.username} onChange={(e) => setCridentials({...cridentials, username  : e.currentTarget.value})} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input 
+    {...register('Email')}
+    
+    type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
   </div>
   <div className="mb-3">
     <label  htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input value={cridentials.password} onChange = {(e) => setCridentials({...cridentials, password : e.currentTarget.value} )} type="password" className="form-control" id="exampleInputPassword1" />
+    <input  {...register('Password')} type="password" className="form-control" id="exampleInputPassword1" />
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
