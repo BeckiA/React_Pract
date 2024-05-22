51a1837006575049f24763b2c99e9a9357e13d71
Bereket Abera <Bereket.Abera@zemenbank.local>
2024-05-22 11:18:17 +0300

    Managing Forms using UseState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,29 @@
-import { FormEvent, useRef } from "react"
+import {FormEvent, useState } from "react"
 
 const Form = () => {
-    
-   const emailRef= useRef<HTMLInputElement>(null);
-   const passRef = useRef<HTMLInputElement>(null);
 
-  const credentials = {
-    email: '',
-    password: ''
-  }
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault()
-        if(emailRef.current !== null)
-            credentials.email = emailRef.current.value
-        if(passRef.current !== null)
-             credentials.password = passRef.current.value
-        console.log(credentials)
-      }
+  const[ cridentials, setCridentials ]= useState(
+    {
+      username: '',
+      password: ''
+    }
+    
+  )
+  const handleSubmit = (e : FormEvent)=>{
+    e.preventDefault();
+    console.log(cridentials)
+
+  } 
+
   return (
     <form onSubmit={handleSubmit}>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input ref={emailRef} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input value={cridentials.username} onChange={(e) => setCridentials({...cridentials, username  : e.currentTarget.value})} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
   </div>
   <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input ref={passRef} type="password" className="form-control" id="exampleInputPassword1" />
+    <label  htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input value={cridentials.password} onChange = {(e) => setCridentials({...cridentials, password : e.currentTarget.value} )} type="password" className="form-control" id="exampleInputPassword1" />
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
